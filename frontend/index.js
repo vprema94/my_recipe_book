4a53598623df7896cefd1d7650d7103b350d1566
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-12 13:45:59 -0500

    ingredient params working

--- frontend/index.js
+++ frontend/index.js
@@ -5,11 +5,15 @@ let array = ['45367385', '11672', '45276886']
 const  API_KEY = '3EIKJr9x60u3tKEl9NOwreStlYavg2CcPmfcZ5ZS'
 const addRecipeBtn = document.querySelector('#new-recipe-btn')
 const recipeForm = document.querySelector('.add-recipe-form')
+const submitBtn = document.querySelector('.submit-button')
 let addRecipe = false
 let recipeContainer = document.querySelector('#recipe-collection')
 let resultsContainer = document.querySelector('#results')
 let searchIngBtn = document.querySelector('#search')
-
+let itemContainer = document.querySelector('#item-container')
+let ingredientNums = []
+let ingredientParams = []
+let amounts = []
 
 function setupPage() {
   // renderRecipes()
@@ -43,8 +47,8 @@ function renderRecipe(recipe) {
     // element.appendChild(recipeIng) 
 
     let delbtn = document.createElement('button') 
-    delbtn.className = 'like-btn'
-    delbtn.textContent = "Like <3"
+    delbtn.className = 'delete-btn'
+    delbtn.textContent = "Delete"
     // delbtn.addEventListener('click', deleteRecipe)
     element.appendChild(delbtn)
   recipeContainer.appendChild(element)
@@ -52,8 +56,6 @@ function renderRecipe(recipe) {
 
 function addFormHandler() {
   addRecipeBtn.addEventListener('click', () => { 
-    recipeForm.addEventListener('submit', submitForm)
-    // hide & seek with the form
     addRecipe = !addRecipe
     if (addRecipe) {
       recipeForm.style.display = 'block'
@@ -61,14 +63,20 @@ function addFormHandler() {
       recipeForm.style.display = 'none'
     }
   }) 
+  recipeForm.addEventListener('submit', submitForm)
 }
 
 function submitForm() {
   event.preventDefault()
   let name = event.target.name.value
   let instructions = event.target.instructions.value
-  // let ingSearch = event.target.ingredient.value
-  // ingredientSearch(ingSearch)
+
+  for (let j = 0; j<itemContainer.children.length; j++) {
+    let amount = itemContainer.children[j].children[0].value + " " + itemContainer.children[j].children[1].value
+    amounts.push(amount)
+  }
+
+  ingredientNums.forEach(no => getIngredientInfo(no)) 
 }
 
 function addSearchHandler() {
@@ -82,25 +90,72 @@ function ingredientSearch(ingredient) {
 }
 
 function renderResults(results) {
-  console.log(results)
   resultsContainer.innerHTML = ""
   results.forEach(item => {
     let searchResult = document.createElement('p')
     // add "add ingredient" pushes ingredient info into an array & renders ingredient name on page under a 'list'
     searchResult.textContent = item.name
+      let ingBtn = document.createElement('button')
+      ingBtn.textContent = "Add Me to Your Recipe!"
+      ingBtn.addEventListener('click', () => renderItem(item))
+      searchResult.appendChild(ingBtn)
     resultsContainer.appendChild(searchResult)
     // submit form will call createIngredients() that takes in the array of ingredients and creates them
     // submit form will also call createRecipe() and creates recipe with name and instructions
   })
 }
 
-function createIngredients(array) {
-  //fetch ingredient info from ndbno
-  array.forEach(ndbno => {
-    ING_URL = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${ndbno}&type=f&format=json&api_key=${API_KEY}`
-    return fetch(ING_URL).then(res => res.json()).then(data => console.log(data.foods[0].food.nutrients[0].measures[0]))
-    createIngredient(name, body, ndbno)
-  })
+function renderItem(item) {
+  resultsContainer.innerHTML= ""
+  let singleItem = document.createElement('p')
+  singleItem.textContent = item.name 
+    let amountInput = document.createElement('input')
+    amountInput.placeholder = 'Enter Amount'
+    singleItem.appendChild(amountInput)
+
+    let amountDrop = document.createElement('select')
+    amountDrop.name = 'measurements'
+      let tbsp = document.createElement('option')
+      tbsp.value = 'tbsp'
+      tbsp.textContent = 'tablespoon'
+      amountDrop.appendChild(tbsp)
+
+      let tsp = document.createElement('option')
+      tsp.value = 'tsp'
+      tsp.textContent = 'teaspoon'
+      amountDrop.appendChild(tsp) 
+
+      let cup = document.createElement('option')
+      cup.value = 'cup'
+      cup.textContent = 'cup(s)'
+      amountDrop.appendChild(cup) 
+
+      let gram = document.createElement('option')
+      gram.value = 'gram'
+      gram.textContent = 'gram(s)'
+      amountDrop.appendChild(gram)
+
+    singleItem.appendChild(amountDrop)
+  itemContainer.appendChild(singleItem)  
+  ingredientNums.push(item.ndbno)
 }
 
-// createIngredients(array)
+
+function getIngredientInfo(no) {
+  ING_URL = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${no}&type=f&format=json&api_key=${API_KEY}`
+  return fetch(ING_URL).then(res => res.json()).then(data => {
+    let name = data.foods[0].food.desc.name
+    let num = data.foods[0].food.desc.ndbno
+    let conv = (data.foods[0].food.nutrients[0].measures[0].eqv / data.foods[0].food.nutrients[0].measures[0].qty).toString() + `${data.foods[0].food.nutrients[0].measures[0].eunit}/${data.foods[0].food.nutrients[0].measures[0].label}` 
+
+    let ingredientInfo = {name: name, ndbno: num, conv: conv}
+    ingredientParams.push(ingredientInfo) 
+
+  }).then(() => addAmounts(ingredientParams))
+}
+
+function addAmounts(ingredientParams) {
+  for (let i = 0; i<amounts.length; i++ ) {
+    ingredientParams[i]['amount'] = amounts[i]  
+  }
+}
