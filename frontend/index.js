c55684848f762e5de79e9f90a20f529e0a0a2ceb
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-12 09:11:43 -0500

    trying to reorganize

--- frontend/index.js
+++ frontend/index.js
@@ -2,51 +2,90 @@ document.addEventListener('DOMContentLoaded', setupPage);
 
 const BASE_URL = 'http://localhost:3000'
 let array = ['45367385', '11672', '45276886']
-const  API_KEY = ''
+const  API_KEY = '3EIKJr9x60u3tKEl9NOwreStlYavg2CcPmfcZ5ZS'
+const addRecipeBtn = document.querySelector('#new-recipe-btn')
+const recipeForm = document.querySelector('.add-recipe-form')
+let addRecipe = false
+let recipeContainer = document.querySelector('#recipe-collection')
+let resultsContainer = document.querySelector('#results')
+let searchIngBtn = document.querySelector('#search')
 
 
 function setupPage() {
-  fetchRecipes().then(function(data) {
+  // renderRecipes()
+  addFormHandler()
+  addSearchHandler()
+}
+
+function renderRecipes() {
+  getRecipes().then(function(data) {
     data.forEach(renderRecipe)
   })
-  addFormHandler()
-}
+} 
 
-function fetchRecipes() {
-  return fetch(BASE_URL + '/recipes').then(res => res.json())
-}
-
+function getRecipes() {
+  return fetch(`${BASE_URL}/recipes`).then(res => res.json())
+} 
 
 function renderRecipe(recipe) {
+  let element = document.createElement('div')
+  element.className = 'card'
+  element.dataset.id = recipe.id
+    let recipeName = document.createElement('h2')
+    recipeName.textContent = recipe.name
+    element.appendChild(recipeName) 
 
+    let recipeIns = document.createElement('p')
+    recipeIns.textContent = recipe.instructions
+    element.appendChild(recipeIns)
+
+    // let recipeIng = document.createElement('p')
+    // element.appendChild(recipeIng) 
+
+    let delbtn = document.createElement('button') 
+    delbtn.className = 'like-btn'
+    delbtn.textContent = "Like <3"
+    // delbtn.addEventListener('click', deleteRecipe)
+    element.appendChild(delbtn)
+  recipeContainer.appendChild(element)
 }
 
 function addFormHandler() {
-  let form = document.querySelector('form')
-  form.addEventListener('submit', submitForm)
+  addRecipeBtn.addEventListener('click', () => { 
+    recipeForm.addEventListener('submit', submitForm)
+    // hide & seek with the form
+    addRecipe = !addRecipe
+    if (addRecipe) {
+      recipeForm.style.display = 'block'
+    } else {
+      recipeForm.style.display = 'none'
+    }
+  }) 
 }
 
 function submitForm() {
   event.preventDefault()
   let name = event.target.name.value
-  let ingSearch = event.target.ingredient.value
   let instructions = event.target.instructions.value
-  ingredientSearch(ingSearch)
-
+  // let ingSearch = event.target.ingredient.value
+  // ingredientSearch(ingSearch)
 }
 
-function ingredientSearch(ingredient) {
+function addSearchHandler() {
+  searchIngBtn.addEventListener('click',ingredientSearch) 
+}
 
-  const ING_SEARCH_URL = `https://api.nal.usda.gov/ndb/search/?format=json&q=${ingredient}&sort=n&offset=0&api_key=${API_KEY}`
-  fetch(ING_SEARCH_URL).then(res => res.json()).then(data => renderResults(data.list.item))
+function ingredientSearch() {
+  let ingredient = event.target.parentElement.children[1].value
+  let ING_SEARCH_URL = `https://api.nal.usda.gov/ndb/search/?format=json&q=${ingredient}&sort=n&offset=0&api_key=${API_KEY}`
+  // fetch(ING_SEARCH_URL).then(res => res.json()).then(data => renderResults(data.list.item))
+  debugger
+  fetch(ING_SEARCH_URL).then(res => res.json()).then(data => console.log(data))
 }
 
 function renderResults(results) {
-  // debugger
   console.log(results)
-  let resultsContainer = document.querySelector('#results')
   results.forEach(item => {
-    // debugger
     let name = document.createElement('p')
     // add "add ingredient" pushes ingredient info into an array & renders ingredient name on page under a 'list'
     name.textContent = item.name
@@ -65,4 +104,4 @@ function createIngredients(array) {
   })
 }
 
-createIngredients(array)
+// createIngredients(array)
